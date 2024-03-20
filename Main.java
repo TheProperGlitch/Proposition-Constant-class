42dd80751f7ff6b2f5191fa5001560b9e36b1644
TheProperGlitch <bvancepantz@gmail.com>
2024-03-20 13:33:53 -0700

    Truth Table
    This took a while because MY COMPUTER BROKE AND I HAD TO RESET EVERYTHING AND LOST ALL MY PROGRESE BECAASUE I ONLY WANT TO MAKE COMMITS WHEN I AMD DONE WIGTHD TRE THINGAFAGEW

--- Main.java
+++ Main.java
@@ -167,6 +167,119 @@ public class Main {
         column = 0;
         }
 
+        System.out.println("TRUTH TABLE: ");
+        //All possible variariations for six truth assignments
+        PropositionConstant xa = new PropositionConstant("a");
+        PropositionConstant xb = new PropositionConstant("b");
+        PropositionConstant xc = new PropositionConstant("c");
+        PropositionConstant xd = new PropositionConstant("d");
+        PropositionConstant xe = new PropositionConstant("e");
+        PropositionConstant xf = new PropositionConstant("f");
+        ArrayList<PropositionConstant> here = new ArrayList<PropositionConstant>();
+        here.add(xa);
+        here.add(xb);
+        here.add(xc);
+        here.add(xd);
+        here.add(xe);
+        here.add(xf);
+        Boolean oa = false;
+        Boolean ob = false;
+        Boolean oc = false;
+        Boolean od = false;
+        Boolean oe = false;
+        Boolean of = false;
+        ArrayList<Boolean> there = new ArrayList<Boolean>();
+        there.add(oa);
+        there.add(ob);
+        there.add(oc);
+        there.add(od);
+        there.add(oe);
+        there.add(of);
+
+        ArrayList<ArrayList<Boolean>> allBools = new ArrayList<ArrayList<Boolean>>();
+        int counting = 0;
+        for (int layerOne = 0; layerOne < 2; layerOne++) {
+          if (layerOne == 0) {
+               oa = true;
+          } else oa = false;
+          for (int layerTwo = 0; layerTwo < 2; layerTwo++) {
+               if (layerTwo == 0) {
+                    ob = true;
+               } else ob = false;
+               for (int layerThree = 0; layerThree < 2; layerThree++) {
+                    if (layerThree == 0) {
+                         oc = true;
+                    } else oc = false;
+                    for (int layerFour = 0; layerFour < 2; layerFour++) {
+                         if (layerFour == 0) {
+                              od = true;
+                         } else od = false;
+                         for (int layerFive = 0; layerFive < 2; layerFive++) {
+                              if (layerFive == 0) {
+                                   oe = true;
+                              } else oe = false;
+                              for (int layerSix = 0; layerSix < 2; layerSix++) {
+                                   if (layerSix == 0) {
+                                        of = true;
+                                   } else of = false;
+                                   there.set(0, oa);
+                                   there.set(1, ob);
+                                   there.set(2, oc);
+                                   there.set(3, od);
+                                   there.set(4, oe);
+                                   there.set(5, of);
+                                   allBools.add((ArrayList<Boolean>)there.clone());
+                                   counting++;
+                                 }
+                            }
+                       }
+                  }
+             }
+        }
+        LogicalSentence ma = new LogicalSentence("a");
+        LogicalSentence mb = new LogicalSentence("b");
+        LogicalSentence mc = new LogicalSentence("c");
+        LogicalSentence md = new LogicalSentence("~a");
+        LogicalSentence me = new LogicalSentence("~b");
+        LogicalSentence mf = new LogicalSentence("~c");
+        Boolean[][] bigArray = new Boolean[16][64];
+
+        LogicalSentence[] sentences = new LogicalSentence[16];
+
+        sentences[0] = new LogicalSentence(false, ma, ma, "&");
+        sentences[1] = new LogicalSentence(false, ma, mb, "&");
+        sentences[2] = new LogicalSentence(false, ma, mc, "&");
+        sentences[3] = new LogicalSentence(false, ma, md, "&");
+        sentences[4] = new LogicalSentence(false, ma, me, "&");
+        sentences[5] = new LogicalSentence(false, ma, mf, "&");
+        sentences[6] = new LogicalSentence(false, mb, mb, "&");
+        sentences[7] = new LogicalSentence(false, mc, mc, "&");
+        sentences[8] = new LogicalSentence(false, mc, mf, "&");
+        sentences[9] = new LogicalSentence(false, ma, md, "|");
+        sentences[10] = new LogicalSentence(false, ma, mb, "|");
+        sentences[11] = new LogicalSentence(false, ma, mc, "|");
+        sentences[12] = new LogicalSentence(false, ma, mc, "<=>");
+        sentences[13] = new LogicalSentence(false, ma, md, "<=>");
+        sentences[14] = new LogicalSentence(false, mb, mc, "=>");
+        sentences[15] = new LogicalSentence(false, mb, me, "=>");
+
+
+        for (int layerOne = 0 ; layerOne < 16 ; layerOne++){
+          for (int layerTwo = 0 ; layerTwo < 64 ; layerTwo++ ){
+               TruthAssignment eval = new TruthAssignment(here, allBools.get(layerTwo));
+               bigArray[layerOne][layerTwo] = sentences[layerOne].evaluate(eval);
+          }
+        }
+        System.out.println("     1      2      3      4      5      6      7      8      9     10     11     12    13     14    15    16");
+        for (int layerOne = 0; layerOne < 64 ; layerOne++){ 
+          System.out.print(layerOne + ": ");
+          for (int layerTwo = 0 ; layerTwo < 16 ; layerTwo++) {
+               System.out.print(bigArray[layerTwo][layerOne] + "| ");
+          }
+          System.out.println();
+        }
+
+
         if (showTest){
         for(int l = 0 ; l < 4 ; l++){
             for (int m = 0; m < 4 ; m++){
